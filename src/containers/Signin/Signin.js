cbe52d885606dab9e502d8880129262e5926a0f3
htkz <htkzmo@gmail.com>
2020-07-06 17:41:08 -0400

    Adjust css style

--- src/containers/Signin/Signin.js
+++ src/containers/Signin/Signin.js
@@ -109,14 +109,9 @@ export default function SignInSide() {
                         >
                             Sign In
                         </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href='#' variant='body2'>
-                                    Forgot password?
-                                </Link>
-                            </Grid>
+                        <Grid container justify='center'>
                             <Grid item>
-                                <Link href='#' variant='body2'>
+                                <Link href='/signup' variant='body2'>
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
