257f546311807a759c0973afbde7307a89b13b2b
htkz <htkzmo@gmail.com>
2020-07-06 16:48:52 -0400

    Add router for signin and signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Signin from './containers/Signin/Signin';
+import Signup from './containers/Signup/Signup';
 import './App.css';
 
 function App() {
     return (
         <div>
             <Switch>
-                <Route path='/' render={() => <p>Welcome!</p>} />
+                <Route path='/signin' exact component={Signin} />
+                <Route path='/signup' exact component={Signup} />
+                <Route path='/' exact render={() => <p>Welcome!</p>} />
+                <Route path='/' render={() => <p>Error: Page not found!</p>} />
             </Switch>
         </div>
     );
