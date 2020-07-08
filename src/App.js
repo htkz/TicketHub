5f058022935a0e9ff601a1102d191600164d22dc
htkz <htkzmo@gmail.com>
2020-07-08 15:35:31 -0400

    Add main page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Signin from './containers/Signin/Signin';
 import Signup from './containers/Signup/Signup';
+import Main from './containers/Main/Main';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <Switch>
                 <Route path='/signin' exact component={Signin} />
                 <Route path='/signup' exact component={Signup} />
+                <Route path='/main' exact component={Main} />
                 <Route path='/' exact render={() => <p>Welcome!</p>} />
                 <Route path='/' render={() => <p>Error: Page not found!</p>} />
             </Switch>
