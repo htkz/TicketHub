1e1f3f54ceca61790ee85bb2283f97451a62f84c
htkz <htkzmo@gmail.com>
2020-07-13 14:34:45 -0400

    Bug fix

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -1,18 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
+    userName: {
+        marginRight: theme.spacing(1),
     },
     title: {
         flexGrow: 1,
@@ -31,10 +29,17 @@ const header = (props) => {
                     <Typography variant='h6' className={classes.title}>
                         TicketHub
                     </Typography>
-                    <Button color='inherit'>Htkz</Button>
-                    <Button color='inherit'>Profile</Button>
-                    <Button color='inherit'>My Account</Button>
-                    <Button color='inherit'>Logout</Button>
+                    <div>
+                        <Typography
+                            variant='button'
+                            className={classes.userName}
+                        >
+                            Htkz
+                        </Typography>
+                        <Button color='inherit'>Profile</Button>
+                        <Button color='inherit'>My Account</Button>
+                        <Button color='inherit'>Logout</Button>
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
